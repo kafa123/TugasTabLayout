7501c1a498d74082e3351444d77a463366c6dab9
kafa123 <nurmuhammadkafabih@mail.ugm.ac.id>
2023-10-23 12:40:38 +0700

    udah selesai

--- app/src/main/java/com/example/tugastablayout/MainActivity2.kt
+++ app/src/main/java/com/example/tugastablayout/MainActivity2.kt
@@ -3,6 +3,8 @@ package com.example.tugastablayout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.tugastablayout.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
@@ -20,5 +22,21 @@ class MainActivity2 : AppCompatActivity() {
         }
 
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.logout, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.logout -> {
+                val intent= Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
